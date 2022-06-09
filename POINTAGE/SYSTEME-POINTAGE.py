f47ca3c398b8254f6a779d70f4ecc35c1cef52c3
Georginho <61295220+DOPEKILL431@users.noreply.github.com>
2022-06-09 18:40:52 +0000

    Update SYSTEME-POINTAGE.py

--- POINTAGE/SYSTEME-POINTAGE.py
+++ POINTAGE/SYSTEME-POINTAGE.py
@@ -1,36 +1,40 @@
+from pydoc import Doc
 from tkinter import*
 from math import*
 import time
 import timeit
 from tkinter import * 
 import tkinter as tk
+from datetime import datetime
 
-def recuperation_entry():
+def valider():
     recup_nom=nom1.get()
     recup_prenom=prenom1.get()
     recup_age=age1.get()
     recup_lieu_de_naissance=lieu_de_naissance1.get()
     recup_raison_de_venue=raison_de_venue1.get()
-    f = open ("class.txt", "a+")    # ouverture
-    f.write("NOM : ")
-    f.write (recup_nom)
-    f.write("|||")
-    f.write("PRENOM : ")
-    f.write(recup_prenom)  
-    f.write("|||") # écriture de la chaîne de caractères  s
-    f.write("AGE : ") # écriture de la chaîne de caractères  s
-    f.write(recup_age)
-    f.write("|||")
-    f.write("LIEU DE NAISSANCE : ")
-    f.write(recup_lieu_de_naissance)
-    f.write("|||")
-    f.write("RAISON DE VENUE : ")
-    f.write(recup_raison_de_venue)
-    f.write("\n")
-    f.close ()
-    print(recup_age,recup_lieu_de_naissance,recup_nom,recup_prenom,recup_raison_de_venue)
+    doc= open ("", "a+") #nom du fichier dans lequel écrire    
+    doc.write("NOM : ")
+    doc.write (recup_nom)
+    doc.write("|||")
+    doc.write("PRENOM : ")
+    doc.write(recup_prenom) 
+    doc.write("|||") 
+    doc.write("AGE : ")
+    doc.write(recup_age)
+    doc.write("|||")
+    doc.write("LIEU DE NAISSANCE : ")
+    doc.write(recup_lieu_de_naissance)
+    doc.write("|||")
+    doc.write("RAISON DE VENUE : ")
+    doc.write(recup_raison_de_venue)
+    doc.write("|||")
+    doc.write(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    doc.write("\n")
+    doc.close()
     
-     # fermeturef = open ("nom-fichier", "w")    # ouverture
+    
+
 def clear():
     nom1.delete(0, END)
     prenom1.delete(0, END)
@@ -39,7 +43,7 @@ def clear():
     raison_de_venue1.delete(0, END)
 
 root = tk.Tk()
-root.geometry("1920x1080")
+root.geometry("360x340")
 root.title("POINTAGE")
 
 
@@ -69,10 +73,12 @@ raison_de_venue.pack()
 raison_de_venue1=Entry(root,width=20)
 raison_de_venue1.pack()
 
+
+
 quitter=Button(root, text="Fermer", command=root.quit)
 quitter.pack()
 
-valider=Button(root,text="Valider",command=recuperation_entry)
+valider=Button(root,text="Valider",command=valider)
 valider.pack()
 
 reinitialiser_les_champs=Button(root,text="Supprimer les champs",command=clear)
